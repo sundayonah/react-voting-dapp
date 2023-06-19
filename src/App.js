6246a00f99a745c16ec826895e72668889d2f15e
sundayonah <sundayonah94@gmail.com>
2023-06-19 10:50:57 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Signer, ethers } from 'ethers';
+import {contractAbi, contractAddress} from "./constant/constant"
+import Login from './components/Login';
+import { useState } from 'react';
 
 function App() {
+
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [isConnected, setIsConnected] = useState(false);
+  const [votingStatus, setVotingStatus] = useState(true);
+  const [remainingTime, setremainingTime] = useState('');
+  const [candidates, setCandidates] = useState([]);
+  const [number, setNumber] = useState('');
+  const [CanVote, setCanVote] = useState(true);
+
+
+async function connectWallet () {
+  if(window.ethereum){
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      setProvider(provider)
+      await provider.send("eth_requestAccounts", [])
+      const signer = provider.getSigner();
+      const address = await signer.getAddress();
+      console.log(`Metamask Connected :${address}`)
+    
+    } catch (error) {
+      console.error(error)
+      
+    }
+  }
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>hello</h1>
+     <Login />
     </div>
   );
 }
